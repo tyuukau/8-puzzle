c6175f80c977f5577c4be41cf15bb0ed9ca42e7d
tyuukau <bachocnhi2002@gmail.com>
2023-11-21 23:44:45 +0700

    [code] ~ AStar class
    PriorityQueue -> list + heapq

--- src/algorithms/astar.py
+++ src/algorithms/astar.py
@@ -1,47 +1,61 @@
-from __future__ import annotations
-from typing import List, Tuple, Set, Optional, Callable
-from algorithms.abstract_search import InformedSearchAlgorithm
+from typing import Callable
+from algorithms.abstract_search import Result, SearchResult, InformedSearchAlgorithm
 from node import Node
-from queue import PriorityQueue
+import heapq
+
 
 class AStar(InformedSearchAlgorithm):
-  """
-  A* search algorithm implementation.
+    """
+    A* search algorithm implementation.
 
-  Args:
-    heuristic (Callable): A function that takes two arguments: the current state and the goal state,
-      and returns the estimated cost to reach the goal state from the current state.
-  """
-  def __init__(self, heuristic: Callable) -> None:
-    super().__init__(heuristic)
+    Args:
+      heuristic (Callable): A function that takes two arguments: the current state and the goal state,
+        and returns the estimated cost to reach the goal state from the current state.
+    """
 
-  def search(self, start: Node, goal: Node) -> Tuple[str, Optional[List[Node]], int, int]:
-    open_list = PriorityQueue()
-    open_list.put((0, start))
-    
-    time_cp = 0
-    space_cp = open_list.qsize()
+    def __init__(self, heuristic: Callable) -> None:
+        super().__init__(heuristic)
 
-    cost_so_far = {}
-    cost_so_far[start.state] = 0
+    def search(self, start: Node, goal: Node) -> SearchResult:
+        frontier = []
+        frontier.append(
+            (start.cost + self.heuristic(start.state, goal.state), -start.cost, start)
+        )
 
-    while not open_list.empty():
-      _, node = open_list.get()
-      
-      time_cp += 1
+        closed = set()
 
-      if self._is_goal(node, goal):
-        path = self._reconstruct_path(node)
-        return "success", path, time_cp, space_cp
+        time_cp = 0
+        space_cp = len(closed) + len(frontier)
 
-      for next_node in node.expand():
-        new_cost = cost_so_far[node.state] + next_node.cost
-        if next_node.state not in cost_so_far or new_cost < cost_so_far[next_node.state]:
-          cost_so_far[next_node.state] = new_cost
-          priority = new_cost + self.heuristic(next_node.state, goal.state)
-          open_list.put((priority, next_node))
-          
-          time_cp += 1
-          space_cp = max(space_cp, len(cost_so_far) + open_list.qsize())
+        while frontier:
+            node = heapq.heappop(frontier)[2]
+            closed.add(node.state)
 
-    return "failure", None, time_cp, space_cp
+            time_cp += 1
+            space_cp = max(space_cp, len(closed) + len(frontier))
+
+            if self._is_goal(node, goal):
+                path = self._reconstruct_path(node)
+                return SearchResult(
+                    result=Result.SUCCESS, path=path, time_cp=time_cp, space_cp=space_cp
+                )
+
+            for child in node.expand():
+                if child.state not in closed:
+                    heapq.heappush(
+                        frontier,
+                        (
+                            child.cost + self.heuristic(child.state, goal.state),
+                            -child.cost,
+                            child,
+                        ),
+                    )
+                    time_cp += 1
+            space_cp = max(space_cp, len(closed) + len(frontier))
+
+        return SearchResult(
+            result=Result.FAILURE, path=None, time_cp=time_cp, space_cp=space_cp
+        )
+
+
+__all__ = ["AStar"]
