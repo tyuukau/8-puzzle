f817b7fde210e56ea144cdc4ff1c039db2b60ebe
tyuukau <bachocnhi2002@gmail.com>
2023-11-21 23:45:53 +0700

    [code] ~ IDS, IDSWithCyclePruning classes

--- src/algorithms/ids.py
+++ src/algorithms/ids.py
@@ -1,111 +1,118 @@
-from __future__ import annotations
 from typing import List, Tuple, Set, Optional, Callable
-from algorithms.abstract_search import UninformedSearchAlgorithm
+from algorithms.abstract_search import Result, SearchResult, UninformedSearchAlgorithm
 from node import Node
 
-from typing import List, Optional, Tuple
-from src.algorithms.UninformedSearchAlgorithm import UninformedSearchAlgorithm
-from src.Node import Node
 
 class IDS(UninformedSearchAlgorithm):
-  """
-  Iterative Deepening Search (IDS) algorithm implementation.
+    """
+    Iterative Deepening Search (IDS) algorithm implementation.
 
-  Args:
-    max_depth : int, optional
-      Maximum depth to search for a solution. Default is 10000.
-  """
-  def __init__(self, max_depth: int=10000) -> None:
-    super().__init__()
-    self.max_depth = max_depth
-    
-  def _dls(self, start: Node, goal: Node, limit: int) -> Tuple[str, Optional[List[Node]], int, int]:
-    frontier = [(start, 0)]
+    Args:
+      max_depth : int, optional
+        Maximum depth to search for a solution. Default is 10000.
+    """
 
-    time_cp = 0
-    space_cp = len(frontier)
+    def __init__(self, max_depth: int = 10000) -> None:
+        super().__init__()
+        self.max_depth = max_depth
 
-    result = "failure"
+    def _dls(self, start: Node, goal: Node, limit: int) -> SearchResult:
+        frontier = [(start, 0)]
 
-    while not self._is_empty(frontier):
-      node, depth = frontier.pop()
+        time_cp = 0
+        space_cp = len(frontier)
 
-      time_cp += 1
+        result = Result.FAILURE
 
-      if self._is_goal(node, goal):
-        result = "success"
-        path = self._reconstruct_path(node)
-        return result, path, time_cp, space_cp
-      if depth > limit:
-        result = "cutoff"
-      else:
-        for child in node.expand():
-          frontier.append((child, depth + 1))
-
-          time_cp += 1
-          space_cp = max(len(frontier), space_cp)
-
-    return result, None, time_cp, space_cp
-
-  def search(self, start: Node, goal: Node) -> Tuple[str, List[Node], int, int]:
-    for depth in range(self.max_depth):
-      result, path, time_cp, space_cp = self._dls(start, goal, limit=depth)
-      if result != "cutoff":
-        return result, path, time_cp, space_cp
-    return result, path, time_cp, space_cp
-
-class IDSWithCyclePruning(UninformedSearchAlgorithm):
-  """
-  Iterative Deepening Search (IDS) algorithm implementation.
-  Includes cycle pruning.
-
-  Args:
-    max_depth : int, optional
-      Maximum depth to search for a solution. Default is 10000.
-  """
-  def __init__(self, max_depth: int=10000) -> None:
-    super().__init__()
-    self.max_depth = max_depth
-
-  def _dls(self, start: Node, goal: Node, limit: int) -> Tuple[str, Optional[List[Node]], int, int]:
-    frontier = [start]
-
-    expanded = []
-
-    time_cp = 0
-    space_cp = len(frontier)
-
-    result = "failure"
-
-    while not self._is_empty(frontier):
-      node = frontier.pop()
-
-      if node in expanded: # Skip nodes that we have already visited
-        continue
-
-      expanded.append(node)
-
-      time_cp += 1
-
-      if self._is_goal(node, goal):
-        result = "success"
-        path = self._reconstruct_path(node)
-        return result, path, time_cp, space_cp
-      if node.cost > limit:
-        result = "cutoff"
-      else:
-        for child in node.expand():
-          if child not in expanded:
-            frontier.append(child)
+        while frontier:
+            node, depth = frontier.pop()
 
             time_cp += 1
-            space_cp = max(len(frontier), space_cp)
 
-    return result, None, time_cp, space_cp
+            if self._is_goal(node, goal):
+                result = Result.SUCCESS
+                path = self._reconstruct_path(node)
+                return SearchResult(
+                    result=result, path=path, time_cp=time_cp, space_cp=space_cp
+                )
 
-  def search(self, start: Node, goal: Node) -> Tuple[str, List[Node], int, int]:
-    for depth in range(self.max_depth):
-      result, path, time_cp, space_cp = self._dls(start, goal, limit=depth)
-      if result != "cutoff":
-        return result, path, time_cp, space_cp
-    return result, path, time_cp, space_cp
+            if depth > limit:
+                result = Result.CUTOFF
+            else:
+                for child in node.expand():
+                    frontier.append((child, depth + 1))
+
+                    time_cp += 1
+                    space_cp = max(len(frontier), space_cp)
+
+        return SearchResult(result=result, path=None, time_cp=time_cp, space_cp=space_cp)
+
+    def search(self, start: Node, goal: Node) -> SearchResult:
+        for depth in range(self.max_depth):
+            search_result = self._dls(start, goal, limit=depth)
+            if search_result.result != Result.CUTOFF:
+                return search_result
+        return search_result
+
+
+class IDSWithCyclePruning(UninformedSearchAlgorithm):
+    """
+    Iterative Deepening Search (IDS) algorithm implementation.
+    Includes cycle pruning.
+
+    Args:
+      max_depth : int, optional
+        Maximum depth to search for a solution. Default is 10000.
+    """
+
+    def __init__(self, max_depth: int = 10000) -> None:
+        super().__init__()
+        self.max_depth = max_depth
+
+    def _dls(self, start: Node, goal: Node, limit: int) -> SearchResult:
+        frontier = [start]
+
+        expanded = []
+
+        time_cp = 0
+        space_cp = len(frontier)
+
+        result = Result.FAILURE
+
+        while frontier:
+            node = frontier.pop()
+
+            if node in expanded:  # Skip nodes that we have already visited
+                continue
+
+            expanded.append(node)
+
+            time_cp += 1
+
+            if self._is_goal(node, goal):
+                result = Result.SUCCESS
+                path = self._reconstruct_path(node)
+                return SearchResult(
+                    result=result, path=path, time_cp=time_cp, space_cp=space_cp
+                )
+            if node.cost > limit:
+                result = Result.CUTOFF
+            else:
+                for child in node.expand():
+                    if child not in expanded:
+                        frontier.append(child)
+
+                        time_cp += 1
+                        space_cp = max(len(frontier), space_cp)
+
+        return SearchResult(result=result, path=None, time_cp=time_cp, space_cp=space_cp)
+
+    def search(self, start: Node, goal: Node) -> SearchResult:
+        for depth in range(self.max_depth):
+            search_result = self._dls(start, goal, limit=depth)
+            if search_result.result != Result.CUTOFF:
+                return search_result
+        return search_result
+
+
+__all__ = ["IDS", "IDSWithCyclePruning"]
