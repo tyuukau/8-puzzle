713d3b46d883f1210593075f0e9f34636021816e
tyuukau <bachocnhi2002@gmail.com>
2023-12-05 08:31:52 +0700

    [code] + IDA (init only)

--- src/algorithms/ida.py
+++ src/algorithms/ida.py
@@ -1,2 +1,17 @@
-from typing import List, Tuple, Set, Optional, Callable
-from algorithms.abstract_search import InformedSearchAlgorithm
+from typing import Callable
+import heapq
+
+from algorithms.abstract_search import Result, SearchResult, InformedSearchAlgorithm
+from node import Node
+from state import State
+
+
+class IDA(InformedSearchAlgorithm):
+    def __init__(self, heuristic: Callable[[State, State], int]) -> None:
+        super().__init__(heuristic)
+
+    def _search(self, start_state: State) -> SearchResult:
+        pass
+
+
+__all__ = ["IDA"]
