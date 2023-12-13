416e440730fc4e5c14d761e3570f0030f652b308
tyuukau <bachocnhi2002@gmail.com>
2023-12-14 00:02:56 +0700

    [code] – IDS algorithm

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -1,6 +1,5 @@
-from .ids import *
 from .astar import *
 from .gbfs import *
 from .rbfs import *
 
-__all__ = ids.__all__ + astar.__all__ + gbfs.__all__ + rbfs.__all__
+__all__ = astar.__all__ + gbfs.__all__ + rbfs.__all__
--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ from sys import exit
 
 from game_config import GameConfig
 from algorithms.abstract_search import SearchAlgorithm
-from algorithms import IDS, IDSWithCyclePruning, AStar, GBFS, RBFS, PRBFS
+from algorithms import AStar, GBFS, RBFS, PRBFS
 from state import State
 from heuristics import *
 
@@ -81,8 +81,8 @@ def game_generator(n: int = 3) -> GameConfig:
 
 def main():
     game_config = GameConfig(
-        start_state=State(3, 7, 4, 8, 5, 6, 2, 0, 1),
-        goal_state=State(0, 1, 2, 3, 4, 5, 6, 7, 8),
+        start_state=State(8, 6, 7, 2, 5, 4, 3, 0, 1),
+        goal_state=State(1, 2, 3, 4, 5, 6, 7, 8, 0),
     )
     algorithm = AStar(heuristic=manhattan_distance)
     g = Game(game_config=game_config, algorithm=algorithm, ignore_solvability=True)
