29ab2935286c078d5946993591b8946c02d1d5c1
tyuukau <bachocnhi2002@gmail.com>
2023-12-14 12:32:20 +0700

    [code] Node -> PNode

--- src/algorithms/rbfs.py
+++ src/algorithms/rbfs.py
@@ -18,10 +18,9 @@ class RBFS(InformedSearchAlgorithm):
     def __init__(self, heuristic: Callable[[State, State], int], limit: int = 1e15) -> None:
         super().__init__(heuristic)
         self.limit = limit
-        self.f_cost = {}
 
     def _rbfs(
-        self, node: Node, limit: int, time_cp: int = 1, space_cp: int = 1
+        self, node: PNode, limit: int, time_cp: int = 1, space_cp: int = 1
     ) -> Tuple[SearchResult, int]:
         if self._is_goal(node):
             path = self._reconstruct_path(node)
@@ -30,38 +29,37 @@ class RBFS(InformedSearchAlgorithm):
         children = []
 
         for child in node.expand():
-            self.f_cost[child] = max(child.cost + self.h_cost(child), self.f_cost[node])
+            child.f_cost = max(child.cost + self.h_cost(child), node.f_cost)
             if node.parent is not None:
                 if child.state == node.parent.state:
-                    self.f_cost[child] = 1e20
+                    child.f_cost = 1e20
             children.append(child)
 
         while True:
-            children.sort(key=lambda node: self.f_cost[node])
-            best = children[0]
+            children.sort(key=lambda node: node.f_cost)
 
-            if self.f_cost[best] > limit:
-                return SearchResult(Result.FAILURE, None, time_cp, space_cp), self.f_cost[best]
+            best = children[0]
+            if best.f_cost > limit:
+                return SearchResult(Result.FAILURE, None, time_cp, space_cp), best.f_cost
 
             alternative = children[1]
-            search_result, best_path = self._rbfs(
-                best, min(limit, self.f_cost[alternative]), time_cp, space_cp
+            search_result, best.f_cost = self._rbfs(
+                best, min(limit, alternative.f_cost), time_cp, space_cp
             )
-            self.f_cost[best] = best_path
             if search_result.result != Result.FAILURE:
-                return search_result, self.f_cost[best]
+                return search_result, best.f_cost
 
     def _search(self, start_state: State) -> SearchResult:
-        start = Node(start_state)
+        start = PNode(start_state)
+        start.f_cost = start.cost + self.h_cost(start)
 
-        self.f_cost[start] = 0
         search_result, _ = self._rbfs(start, self.limit)
         return search_result
 
 
 class PRBFS(InformedSearchAlgorithm):
     """
-    Recursive Best First Search (RBFS) algorithm implementation using PNode.
+    Recursive Best First Search (RBFS) algorithm implementation.
 
     Args:
     - `heuristic` (`Callable[[State, State], int]`): The heuristic function.
